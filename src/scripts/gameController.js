0ad2a349339c06fa9cece23e2d782d398b1340b8
Ben Braun <benjamin.braun2003@gmail.com>
2026-02-02 00:36:09 -0500

    Add and Test switchPlayerTurn()

--- src/scripts/gameController.js
+++ src/scripts/gameController.js
@@ -7,8 +7,16 @@ export class GameController {
 	constructor(playerOne = 'PlayerOne', playerTwo = 'Player2') {
 
 		this.players = [new Player(playerOne, 'real'), new Player(playerTwo, 'real')];
-
 		this.curPlayer = this.players[0];
+	}
+
+	switchPlayerTurn() {
+
+		if (this.curPlayer === this.players[0]) {
+			this.curPlayer = this.players[1];
+		} else if (this.curPlayer === this.players[1]) {
+			this.curPlayer = this.players[0]
+		}
 
 	}
 }
